5e3cc5b20d882a8fbcde892be5f763533b3d34a8
Chris Racioppo <Gravedigger7789@gmail.com>
2024-11-10 14:10:28 -0500

    Commented out unused functions

--- Scenes/desk.gd
+++ Scenes/desk.gd
@@ -1,20 +1,20 @@
 extends Area2D
 class_name Desk
 
-signal desk_selected()
+#signal desk_selected()
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var shader: ShaderMaterial = sprite_2d.material
 
-func _on_mouse_entered() -> void:
-	highlight(true)
-
-func _on_mouse_exited() -> void:
-	highlight(false)
-
-func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseButton && event.is_released():
-		desk_selected.emit()
+#func _on_mouse_entered() -> void:
+	#highlight(true)
+#
+#func _on_mouse_exited() -> void:
+	#highlight(false)
+#
+#func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	#if event is InputEventMouseButton && event.is_released():
+		#desk_selected.emit()
 
 func highlight(enabled: bool) -> void:
 	shader.set_shader_parameter("enabled", enabled)
