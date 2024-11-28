e2d1b9c3025080067d776dfcb91836af5a05bcb4
Chris Racioppo <Gravedigger7789@gmail.com>
2024-11-27 22:24:49 -0500

    Adding game over

--- Levels/class_room.gd
+++ Levels/class_room.gd
@@ -5,12 +5,12 @@ extends Node2D
 @onready var grid_position: Marker2D = $GridPosition
 @onready var grid: Grid = $GridPosition/Grid
 @onready var reset_timer: Timer = $ResetTimer
-
 @onready var heart_container: HeartContainer = $HeartContainer
 
+const GAME_OVER = preload("res://Levels/game_over.tscn")
 const GRID = preload("res://Scenes/grid.tscn")
 
-var max_lives := 6
+@export var max_lives := 3
 var current_lives := max_lives: set = set_current_lives
 func set_current_lives(value: int) -> void:
 	current_lives = value
@@ -46,11 +46,14 @@ func _on_note_found(note: Note) -> void:
 		await teacher_assistant.arrived_at_note
 	reset_timer.start()
 	await reset_timer.timeout
-	if is_instance_valid(grid_position):
-		var children := grid_position.get_children()
-		for child in children:
-			child.queue_free()
-		grid = GRID.instantiate()
-		grid_position.add_child(grid)
-		teacher_assistant.grid = grid
-		setup()
+	if current_lives > 0:
+		if is_instance_valid(grid_position):
+			var children := grid_position.get_children()
+			for child in children:
+				child.queue_free()
+			grid = GRID.instantiate()
+			grid_position.add_child(grid)
+			teacher_assistant.grid = grid
+			setup()
+	else:
+		get_tree().change_scene_to_packed(GAME_OVER)
---
+++ Levels/game_over.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var CLASS_ROOM := "res://Levels/class_room.tscn"
+
+func _on_restart_button_pressed() -> void:
+	get_tree().change_scene_to_file(CLASS_ROOM)
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
