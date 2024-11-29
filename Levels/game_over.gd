c62ed32998561ed12967638602ae48229158a393
Chris Racioppo <Gravedigger7789@gmail.com>
2024-11-29 17:05:30 -0500

    removing quit button on web

--- Levels/game_over.gd
+++ Levels/game_over.gd
@@ -1,6 +1,16 @@
 extends Control
 
 var CLASS_ROOM := "res://Levels/class_room.tscn"
+@onready var quit_button: Button = %QuitButton
+
+func _ready() -> void:
+	match OS.get_name():
+		"Windows","macOS", "Linux":
+			quit_button.disabled = false
+			quit_button.show()
+		"Android", "iOS", "Web", _:
+			quit_button.hide()
+			quit_button.disabled = true
 
 func _on_restart_button_pressed() -> void:
 	get_tree().change_scene_to_file(CLASS_ROOM)
