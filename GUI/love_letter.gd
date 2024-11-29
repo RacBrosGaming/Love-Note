e8f197f52b790740eaac1880c713144baf0607a1
Chris Racioppo <Gravedigger7789@gmail.com>
2024-11-29 13:39:41 -0500

    show letter from current postion

--- GUI/love_letter.gd
+++ GUI/love_letter.gd
@@ -16,15 +16,17 @@ signal chose_answer(said_yes: bool)
 @onready var letter_back_starting_scale := letter_back.scale
 
 var note_position: Vector2
+var time_to_show := 2.0 
+var time_to_write := 5.0
 
 func write(target_position: Vector2) -> void:
 	note_position = target_position
 	no_check_box.visible = false
 	yes_check_box.visible = false
 	letter_words.visible_ratio = 0
-	show()
+	await show_letter(note_position)
 	var word_tween := create_tween()
-	word_tween.tween_property(letter_words, "visible_ratio", 1, 5)
+	word_tween.tween_property(letter_words, "visible_ratio", 1, time_to_write)
 	await word_tween.finished
 	flip_timer.start()
 
@@ -33,7 +35,7 @@ func present_option(target_position: Vector2) -> void:
 	no_check_box.show()
 	yes_check_box.show()
 	yes_check_box.grab_focus()
-	show()
+	show_letter(note_position)
 
 func _on_yes_check_box_toggled(toggled_on: bool) -> void:
 	if toggled_on:
@@ -61,15 +63,30 @@ func _on_flip_timer_timeout() -> void:
 	await send_letter_to_desk()
 	finished_writing.emit()
 
+func show_letter(from_position: Vector2) -> void:
+	hide()
+	word_container.hide()
+	letter_back.show()
+	global_position = from_position
+	letter_back.scale = Vector2(1, 1)
+	show()
+	var position_tween := create_tween()
+	position_tween.tween_property(self, "global_position", starting_position, time_to_show)
+	var scale_tween := create_tween()
+	scale_tween.tween_property(letter_back, "scale", letter_back_starting_scale, time_to_show)
+	await scale_tween.finished
+	word_container.show()
+	letter_back.hide()
+
 func send_letter_to_desk() -> void:
 	send_to_desk_timer.start()
 	await send_to_desk_timer.timeout
 	word_container.hide()
 	letter_back.show()
 	var position_tween := create_tween()
-	position_tween.tween_property(self, "global_position", note_position, 2)
+	position_tween.tween_property(self, "global_position", note_position, time_to_show)
 	var scale_tween := create_tween()
-	scale_tween.tween_property(letter_back, "scale", Vector2(1, 1), 2)
+	scale_tween.tween_property(letter_back, "scale", Vector2(1, 1), time_to_show)
 	await scale_tween.finished
 	hide()
 	word_container.show()
