0c0cdfe044f96ec90f275b683a713ea7ef298a21
Chris Racioppo <Gravedigger7789@gmail.com>
2024-11-30 16:27:08 -0500

    added bell cheat

--- Levels/main_menu.gd
+++ Levels/main_menu.gd
@@ -2,11 +2,31 @@ extends Control
 
 const CLASS_ROOM := "res://Levels/class_room.tscn"
 
+@export var game_stats: GameStats
+
 @onready var bully_desk: DeskBully = $BullyDesk
 @onready var play_button: Button = %PlayButton
+@onready var bell: Polygon2D = $Bell
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
+var bell_activate_count := 5
+var bell_click_count := 0
+var bell_activated := false
 
 func _ready() -> void:
+	gui_input.connect(_on_gui_input)
 	bully_desk.taunt()
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file(CLASS_ROOM)
+
+func _on_gui_input(event: InputEvent) -> void:
+	if (event is InputEventMouseButton || event is InputEventScreenTouch) && event.is_released():
+		var bell_clicked := Geometry2D.is_point_in_polygon(bell.to_local(event.position), bell.polygon)
+		if bell_clicked:
+			bell_click_count += 1
+		if !bell_activated && bell_click_count >= 5:
+			bell_activated = true
+			game_stats.max_lives = 6
+			game_stats.time_remaining = 180
+			audio_stream_player.play()
